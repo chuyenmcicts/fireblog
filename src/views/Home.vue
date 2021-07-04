d349e51986e0801f11133cb31bc2c66adf67f1ed
ChuyenMC <chuyenmc@icts.vn>
2021-07-04 18:02:36 +0700

    blog card component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="home">
     <BlogPost :post="welcomeScreen" />
-    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index" />
+    <BlogPost
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+    />
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import BlogPost from '../../components/BlogPost.vue'
+import BlogPost from "../../components/BlogPost.vue";
+import BlogCard from "../../components/BlogCard.vue";
 export default {
   name: "Home",
   components: {
-    BlogPost
+    BlogPost,
+    BlogCard
   },
   data() {
     return {
@@ -33,7 +47,39 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        {
+          blogTitle: "Blog Card #1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "Jul 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "Jul 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "Jul 4, 2021",
+        },
+        {
+          blogTitle: "Blog Card #4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "Jul 4, 2021",
+        },
+      ],
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+</style>
